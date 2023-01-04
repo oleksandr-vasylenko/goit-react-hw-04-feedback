9431642f3cee05b2db82fd55220f921fac9c35c2
oleksandr-vasylenko <oleksandr.vasylenko.ua@gmail.com>
2023-01-04 13:31:40 +0200

    Update App.styled.js

--- src/components/App/App.styled.js
+++ src/components/App/App.styled.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 
 export const Card = styled.section`
-    display: flex;
-    flex-direction: column;
-    gap 24px;
-    
-    width: 400px;
-    margin: auto;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+
+  width: 400px;
+  margin: auto;
 `;
